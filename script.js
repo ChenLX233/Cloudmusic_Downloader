a76725f26e385a9e6494cef5c066fcff0559be96
LxChen <114341596+ChenLX233@users.noreply.github.com>
2025-09-24 22:30:16 +0800

    Change API base URL to new endpoint

--- script.js
+++ script.js
@@ -29,8 +29,7 @@ let allPlaylistMap = {};           // 当前页所有歌单对象映射
 let allSongIdsInPlaylist = [];     // 当前歌单所有歌曲ID（用于歌单详情页全选）
 let lastSongList = [];             // 当前页歌曲列表缓存
 
-// const apiBase = 'https://api.toubiec.cn/wyapi/';       // 自建API根地址(暂时不用)
-const apiBase = 'https://api.toubiec.cn/wyapi/getMusicUrl.php';//（暂时取用）
+const apiBase = 'https://musicapi.lxchen.cn';       // 自建API根地址
 const cloudApi = 'https://163api.qijieya.cn';      // 云API
 
 // =======================
